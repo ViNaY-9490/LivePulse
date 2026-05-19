f73dc1fd67dfb6563507dabbbb4fe447072a5170
ViNaY-9490 <mr.vinay9490@gmail.com>
2026-05-19 05:49:10 +0530

    added frontend url in the allowed origins

--- MyLiveStream/server/server.js
+++ MyLiveStream/server/server.js
@@ -168,6 +168,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5173',
   'http://localhost:5174',
+  'https://the-live-pulse.vercel.app',
   'https://thelivepulse.netlify.app',
   'http://127.0.0.1:5174',
   ...CORS_ORIGINS,
